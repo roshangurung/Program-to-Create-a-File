45918a250b39674e5c4842a8b3b11043972b8933
roshangurung <r.gurung1992@gmail.com>
2019-12-10 21:25:07 -0800

    Initial commit

---
+++ Program to Create a File/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+class Test///This C# program is used to create a file. Using Create() function the file is created and the content is written to the file. Using while loop check the file is not equal to null. If the condition is true then execute the statement and print the same content.///
+{
+    public static void Main()
+    {
+        string textpath = @"c:\sri\test.txt";
+        using (FileStream fs = File.Create(textpath))
+        {
+            Byte[] info = new UTF8Encoding(true).GetBytes("File is Created");
+            fs.Write(info, 0, info.Length);
+        }
+        using (StreamReader sr = File.OpenText(textpath))
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        Console.Read();
+    }
+}
+///Output:File is Created//
